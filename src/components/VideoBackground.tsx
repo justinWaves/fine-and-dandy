9aab4f5ad33bd05c367e44275221c89a2ba0c9cb
Justin Weisberg <juhstinn@gmail.com>
2025-07-20 18:43:12 -0700

    ✅ Implement bulletproof iOS-compatible video background with 16:9 aspect ratio hack and center transforms

--- src/components/VideoBackground.tsx
+++ src/components/VideoBackground.tsx
@@ -2,14 +2,16 @@
 
 export default function VideoBackground() {
   return (
-    <div className="fixed inset-0 w-full h-screen z-[-1] overflow-hidden">
-      <iframe
-        src="https://player.vimeo.com/video/173166268?background=1&autoplay=1&loop=1&byline=0&title=0&muted=1"
-        frameBorder="0"
-        allow="autoplay; fullscreen"
-        allowFullScreen
-        className="absolute top-0 left-0 w-[120vw] h-[120vh] min-w-full min-h-screen object-cover scale-[1.2]"
-      />
+    <div className="fixed inset-0 z-[-1] overflow-hidden bg-black">
+      <div className="absolute top-1/2 left-1/2 w-[177.77vh] h-[100vh] -translate-x-1/2 -translate-y-1/2">
+        <iframe
+          src="https://player.vimeo.com/video/173166268?background=1&autoplay=1&loop=1&byline=0&title=0&muted=1"
+          className="w-full h-full"
+          allow="autoplay; fullscreen"
+          allowFullScreen
+          frameBorder="0"
+        />
+      </div>
     </div>
   );
 } 
